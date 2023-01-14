1d612eedd473c80d06e11e3b4c0db65819cc5166
emin <eminyetim25@gmail.com>
2023-01-15 02:59:59 +0300

    Add

--- Program.cs
+++ Program.cs
@@ -6,7 +6,28 @@ namespace GenericList
     {
         public static void Main(string[] args)
         {
+           List<int> sayiListesi = new List<int>();
+           sayiListesi.Add(23);
+           sayiListesi.Add(10);
+           sayiListesi.Add(4);
+           sayiListesi.Add(5);
+           sayiListesi.Add(92);
+
+
+           List<string> renkListesi = new();
+
+           renkListesi.Add("Kırmızı");
+           renkListesi.Add("Mavi");
+           renkListesi.Add("Turuncu");
+           renkListesi.Add("Sarı");
+           renkListesi.Add("Yeşil");
+
+
+
+
            
+          
+
         }
     }
 }
