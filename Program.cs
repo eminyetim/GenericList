d8a219d67cb4312c59bcf944b838941e3cd882ee
emin <eminyetim25@gmail.com>
2023-01-15 03:37:48 +0300

    ListObject

--- Program.cs
+++ Program.cs
@@ -51,8 +51,61 @@ namespace GenericList
           
             //Converting Array to List
             string[] Hayvanlar = {"Kedi","Köpek","Kuş"};
-            List<string> hayvanlarListesi = new List<string>();
+            List<string> hayvanlarListesi = new List<string>(Hayvanlar);
+
+            //Clear
+            hayvanlarListesi.Clear();
+
+            //List of Object
+
+            List<Kullanicilar> kullanicilarListesi = new List<Kullanicilar>();
+            Kullanicilar kullanici1 = new Kullanicilar();
+            Kullanicilar kullanici2 = new Kullanicilar();
+
+            kullanici1.Isim = "ali";
+            kullanici1.Soyisim = "ytm";
+            kullanici1.Yas = 61;
+
+            kullanici2.Isim = "veli";
+            kullanici2.Soyisim = "ytm";
+            kullanici2.Yas = 16;
+
+            kullanicilarListesi.Add(kullanici1);
+            kullanicilarListesi.Add(kullanici2);
+
+
+            foreach (var item in kullanicilarListesi)
+            {
+                System.Console.WriteLine(item.Isim + item.Soyisim + item.Yas);
+            }
+
+            List<Kullanicilar> yeniList = new List<Kullanicilar>();
+            yeniList.Add(new Kullanicilar()
+            {
+                Isim = "ysf",
+                Soyisim = "clk",
+                Yas = 24   
+            });
+
+            foreach (var item in yeniList)
+            {
+                System.Console.WriteLine(item.Isim + item.Soyisim + item.Yas);
+            }
+
+            yeniList.Clear();
+            kullanicilarListesi.Clear();
 
         }
+    }
+
+    class Kullanicilar
+    {
+        private string isim;
+        private string soyisim;
+        private int yas;
+
+        public string Isim { get => isim; set => isim = value; }
+        public string Soyisim { get => soyisim; set => soyisim = value; }
+        public int Yas { get => yas; set => yas = value; }
     }
 }
