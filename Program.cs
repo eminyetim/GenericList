8cbf2802bb727a0f3cf3f1f8489ad9d7eec100e4
emin <eminyetim25@gmail.com>
2023-01-15 03:09:19 +0300

    Foreach

--- Program.cs
+++ Program.cs
@@ -6,21 +6,39 @@ namespace GenericList
     {
         public static void Main(string[] args)
         {
-           List<int> sayiListesi = new List<int>();
-           sayiListesi.Add(23);
-           sayiListesi.Add(10);
-           sayiListesi.Add(4);
-           sayiListesi.Add(5);
-           sayiListesi.Add(92);
+            List<int> sayiListesi = new List<int>();
+            sayiListesi.Add(23);
+            sayiListesi.Add(10);
+            sayiListesi.Add(4);
+            sayiListesi.Add(5);
+            sayiListesi.Add(92);
 
 
-           List<string> renkListesi = new();
+            List<string> renkListesi = new();
 
-           renkListesi.Add("Kırmızı");
-           renkListesi.Add("Mavi");
-           renkListesi.Add("Turuncu");
-           renkListesi.Add("Sarı");
-           renkListesi.Add("Yeşil");
+            renkListesi.Add("Kırmızı");
+            renkListesi.Add("Mavi");
+            renkListesi.Add("Turuncu");
+            renkListesi.Add("Sarı");
+            renkListesi.Add("Yeşil");
+
+
+            System.Console.WriteLine(sayiListesi);
+            System.Console.WriteLine(renkListesi);
+
+            System.Console.WriteLine("--SayiListesi--");
+            // foreach (var item in sayiListesi)
+            //     System.Console.WriteLine(item);
+            sayiListesi.ForEach(sayi => System.Console.WriteLine(sayi));
+            
+            System.Console.WriteLine("\n--RenkListesi--");
+            // foreach (var item in renkListesi)
+            //     System.Console.WriteLine();
+            renkListesi.ForEach(renk => System.Console.WriteLine(renk));
+
+            
+
+
 
 
 
